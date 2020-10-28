7ee51ba5cd900842b266d02b8abbc60ddf575f77
ShchcerbanMariya <shcherban.maryya@gmail.com>
2020-10-28 22:36:58 +0200

    finished

---
+++ lab1/task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write("Wrte x: ");
+            double x=double.Parse(Console.ReadLine());
+
+            Console.Write("Wrte y: ");
+            double y=double.Parse(Console.ReadLine());
+
+            Console.Write("Wrte z: ");
+            double z=double.Parse(Console.ReadLine());
+
+            if (z==0)
+            Console.WriteLine("Error");
+            else
+            {
+            if (Math.Sin(Math.PI+x)==0)
+            Console.WriteLine("Error");
+            else 
+            {
+            double a = Math.Pow(Math.Sin((x+y)/z), 2)/(2*Math.Sin(Math.PI+x));
+                if (a==0)
+                Console.WriteLine("Error");
+                else 
+                {
+                    if (x<0 && a%1!=0)
+                    Console.WriteLine("Error");
+                    else 
+                    {
+                        if(a+z<=0)
+                        Console.WriteLine("Error");
+                        else {
+                        double b = Math.Log(a+z)/(a*a)+1/Math.Pow(x,a);
+                        Console.WriteLine("a={0}, b={1}", a, b);
+                        }
+                    }
+                }
+
+            }
+            }
+        }
+    }
+}
---
+++ lab1/task2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+         static int countdays (int m, int d)
+         {
+            int result=0;
+        
+            for (int i=1; i<m; i++) 
+                {
+                    if (i==2)
+                        result += 28;
+                    else 
+                        result += monthdays(i);
+                }
+            return result+d;
+            
+         }
+               
+        static int monthdays (int m)
+        {
+            if (m >=8)
+                m++;
+            if (m % 2 == 1)
+                return 31;
+            else
+                return 30;
+        }
+        static bool checkInput(int d, int m)
+        {
+            if (d>0 && d<=31 && m>0 && m<=12)
+            {
+                if (m==2 && d<29)
+                    return true;
+                else  
+                {
+                    if (m==2 && d>28)
+                        return false;
+                    if (d<=monthdays(m))
+                        return true;
+                    else 
+                        return false;
+                }
+            }
+            else 
+                return false;
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Write the day ");
+            int d = int.Parse(Console.ReadLine());
+
+            Console.Write("Write the month ");
+            int m = int.Parse(Console.ReadLine());
+
+                                        
+            bool check = checkInput(d, m);
+            if (check == false)
+                Console.WriteLine("Incorrect input");
+            else
+            {
+               
+                int week = countdays(m,d)%7;
+                
+
+                if (week == 1)
+                    Console.WriteLine("{0}.{1} is wednesday", d,m);
+                else if (week == 2)
+                    Console.WriteLine("{0}.{1} is thursday", d,m);
+                else if (week == 3)
+                    Console.WriteLine("{0}.{1} is friday", d,m);
+                else if (week == 4)
+                    Console.WriteLine("{0}.{1} is saturday", d,m);
+                else if (week == 5)
+                    Console.WriteLine("{0}.{1} is sunday", d,m);
+                else if (week == 6)
+                    Console.WriteLine("{0}.{1} is monday", d,m);
+                else Console.WriteLine("{0}.{1} is tuesday", d,m);
+                                               
+                }
+
+            
+
+
+                
+
+        }
+    }
+}
